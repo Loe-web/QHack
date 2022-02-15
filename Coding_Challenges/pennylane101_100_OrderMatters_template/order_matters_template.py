f275d8df31086dcf9ed3982c4c00379583fe1e54
Lorraine Tsitsi <54618257+Loe-web@users.noreply.github.com>
2022-02-15 11:06:10 +0200

    Update order_matters_template.py

--- Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
+++ Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
@@ -18,6 +18,15 @@ def compare_circuits(angles):
     # QHACK #
 
     # define a device and quantum functions/circuits here
+    dev = qml.device('default.qubit', wires=5)
+
+    def circuit1(params):
+        qml.RX(params[0], wires=0)
+        qml.RY(params[1], wires=0)
+        return qml.expval(qml.PauliZ(0))
+    def circuit2(params):
+        qml.RY(params[0], wires=0)
+        qml.Rx(params[1], wires=0)
 
     # QHACK #
 
